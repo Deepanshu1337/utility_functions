278d971f566d22ec401f9d16cc90d392f2a6f17f
Deepanshu Dixit <deepanshudixit07@gmail.com>
2024-12-09 18:37:53 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 //  truncate text by characters including spaces
 function truncateTextByCharacters(text, charLimit = 50) {
-    if (text.length > charLimit) {
-      return text.slice(0, charLimit) + '...';
-    }
-    return text;
+  if (text.length > charLimit) {
+    return text.slice(0, charLimit) + '...';
   }
+  return text;
+}
+
+// truncate by words
+export function truncateTextByWords(text, wordLimit = 10) {
+  const words = text.split(/\s+/);
+  return words.length > wordLimit
+    ? words.slice(0, wordLimit).join(' ') + '...'
+    : text;
+}
